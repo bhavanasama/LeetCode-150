46592c9707a0f611f7b332ed23635ba3496438e3
bhavanasama <samabhavana2004@gmail.com>
2024-10-25 23:20:32 +0530

    26. Remove Duplicates from Sorted Array

--- Remove_Duplicates_Sorted_Array_I.java
+++ Remove_Duplicates_Sorted_Array_I.java
@@ -1,5 +1,31 @@
+import java.util.*;
 class Remove_Duplicates_Sorted_Array_I{
     public static void main(String[] args) {
-        System.out.println("Hi");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the size of the array:");
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        System.out.println("Enter the elements of the array:");
+        for(int i=0;i<n;i++){
+            arr[i] = sc.nextInt();
+            }
+            int remaining = removeDuplicates(arr);
+            System.out.println(remaining);
+            sc.close();
+    }
+    public static int removeDuplicates(int[] nums) {
+        
+        
+        if (nums.length == 0) return 0; 
+        
+        int count = 1; 
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] != nums[i - 1]) { 
+                nums[count] = nums[i];
+                count++; 
+            }
+        }
+        
+        return count; 
     }
 }
